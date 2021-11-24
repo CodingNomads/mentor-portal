951b7eb2c5dc86214add38ff76c64573052ba014
Jon <jon@sessionsdev.com>
2021-11-24 14:46:53 -0500

    Initial commit of app scaffolding

---
+++ src/main/kotlin/com/codingnomads/mentor_portal_api/MentorPortalApiApplication.kt
@@ -0,0 +1,11 @@
+package com.codingnomads.mentor_portal_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MentorPortalApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<MentorPortalApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/codingnomads/mentor_portal_api/MentorPortalApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.codingnomads.mentor_portal_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MentorPortalApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
