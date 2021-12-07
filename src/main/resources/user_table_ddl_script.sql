d88f2af8fbe374597663f9d1b7f3dc5474fdbfc4
Jonathan Sessions <sessionsdev@pop-os.localdomain>
2021-12-07 17:15:22 -0500

    MPA-1: fixed sql script and dockerized dev db

--- src/main/resources/user_table_ddl_script.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `kotlin.backend.db`.`user`;
-
-CREATE TABLE `kotlin.backend.db`.`user` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'PK',
-  `role_code` int(10) UNSIGNED NOT NULL COMMENT 'The user role',
-  `status_code` int(10) NOT NULL,
-  `timezone_offset` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=compressed COMMENT='List of users';
