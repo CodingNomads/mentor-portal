b1fc541971c2893d86c06b56dee41d4d417ac28b
Scott Ford <swford@ualberta.ca>
2022-05-10 14:31:59 -0600

    Update and rename SecurityHandler.kt to UserSecurityHandler.kt
    Changes to UserSecurityHandler.kt to fix merge conflict

---
+++ service/mentor-portal-api/src/main/kotlin/com/codingnomads/mentor_portal_api/handler/UserSecurityHandler.kt
@@ -0,0 +1,36 @@
+package com.codingnomads.mentor_portal_api.handler
+
+import com.codingnomads.mentor_portal_api.entity.ApplicationUser
+import com.codingnomads.mentor_portal_api.mapper.SecurityMapper
+import com.codingnomads.mentor_portal_api.security.ApplicationUserRole
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class UserSecurityHandler(@Autowired private val securityMapper: SecurityMapper) :UserDetailsService {
+    override fun loadUserByUsername(username: String?): ApplicationUser {
+        if (username == null) {
+            throw UsernameNotFoundException("User Not Found")
+        }
+
+        val userSecurityDTO = securityMapper.getUserSecurityDetails(email = username)
+
+        var grantedAuthorities = ApplicationUserRole.MENTOR.getGrantedAuthorities();
+        if (userSecurityDTO.isAdmin) {
+            grantedAuthorities = ApplicationUserRole.ADMIN.getGrantedAuthorities()
+        }
+
+        return ApplicationUser(
+            grantedAuthorities = grantedAuthorities.toMutableList(),
+            userId = userSecurityDTO.userId,
+            password = userSecurityDTO.passwordHash,
+            username = userSecurityDTO.email,
+            isAccountNonExpired = true,
+            isAccountNonLocked = true,
+            isCredentialsNonExpired = true,
+            isEnabled = true)
+    }
+
+}
