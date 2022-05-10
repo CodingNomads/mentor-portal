a22a13bc16700978d254944829300df1b285382a
Scott Ford <swford@ualberta.ca>
2022-05-10 14:45:15 -0600

    cleaned up imports

--- service/mentor-portal-api/src/main/kotlin/com/codingnomads/mentor_portal_api/security/JwtUsernameAndPasswordAuthenticationFilter.kt
+++ service/mentor-portal-api/src/main/kotlin/com/codingnomads/mentor_portal_api/security/JwtUsernameAndPasswordAuthenticationFilter.kt
@@ -8,9 +8,7 @@ import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import java.time.LocalDate
 import java.time.ZonedDateTime
 import java.util.*
 import javax.servlet.FilterChain
